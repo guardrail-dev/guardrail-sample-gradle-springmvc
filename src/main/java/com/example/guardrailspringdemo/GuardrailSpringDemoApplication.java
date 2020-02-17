0d6fb7157496ffe4f8c6ec86450eab50aa3baf2d
Devon Stewart <dstewart@twilio.com>
2020-02-17 12:18:14 -0800

    Spring boilerplate generatr

---
+++ src/main/java/com/example/guardrailspringdemo/GuardrailSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.guardrailspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuardrailSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuardrailSpringDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/guardrailspringdemo/GuardrailSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.guardrailspringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuardrailSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
